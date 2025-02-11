2aa6d48bd70eb107bd5b6676b02dbe6cdfe9732a
sanjay-perisync <sanjay@perisync.work>
2025-02-11 11:29:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import StartPage from "./Components/StartPage";
+import Login from "./Components/Login";
+import Home from "./Components/Home";
 
 function App() {
   return (
-    <>
-   <h1 className="text-red-500">
-    heloo</h1>
-    </>
+    <Routes>
+    
+      <Route
+        path="/"
+        element={
+          <div className="App bg-black">
+           <StartPage/>
+          </div>
+        }
+      />
+
+      {/* Login Page  */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
   );
 }
 
